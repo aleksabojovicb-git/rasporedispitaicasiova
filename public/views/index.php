dc7a79d81d0fd339bc8a2bd9ca3dca44c11bba69
ajsadcc <ajsaajsica2004@gmail.com>
2025-11-18 18:10:06 +0100

    Dodatak cssa za admin

--- public/views/index.php
+++ public/views/index.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Početna stranica</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/index.css">
 </head>
 
 <body>
